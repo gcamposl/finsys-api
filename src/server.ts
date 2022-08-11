0c3068482440fa90480d50b27594ae17bd019472
gcamposl <gcamposlopes@gmail.com>
2022-08-10 23:02:12 -0300

    fixx shit content-type

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,17 @@
-import express from 'express';
-import entryRoutes from './controllers/EntryController';
+import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const router = express();
 router.use(express.json());
-router.use('/entry', entryRoutes);
-
 router.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
+});
+
+router.post('/', (req: Request, res: Response) => {
+  const data = req.body;
+  console.log(data);
+
+  return res.json({ mes: data });
 });
