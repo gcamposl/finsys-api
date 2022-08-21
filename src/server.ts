21de07f8f1137ae665d09dfe70b631270e43f34e
gcamposl <gcamposlopes@gmail.com>
2022-08-21 14:33:20 -0300

    get and post ok

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,6 @@ import dotenv from 'dotenv';
 import path from 'path';
 import fakeDb from './data.json';
 import fs from 'fs';
-import { v4 as uuid } from 'uuid';
 import { MongoClient } from 'mongodb';
 
 enum Type {
@@ -28,30 +27,13 @@ router.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
 
-// consuming json file with fake db
+// consuming json file with fake db [descontinuos]
 let data = fs.readFileSync(path.join(__dirname, 'data.json'));
 
 // connect databsae
-
 const client = new MongoClient(process.env.DB_CONN_STRING);
-
-// async function run() {
-//   try {
-//     // const client = new MongoClient(process.env.DB_CONN_STRING);
-//     // const db = client.db(process.env.DB_NAME);
-//     const database = client.db(process.env.DB_NAME);
-//     const accounts = database.collection('accounts');
-//     const query = { description: 'xpto' };
-//     const description = await accounts.findOne(query);
-//     console.log(description);
-//   } finally {
-//     await client.close();
-//   }
-// }
-
-// run();
-
-const connectDB = () => {
+const connectDB = async () => {
+  await client.connect();
   const database = client.db(process.env.DB_NAME);
   return database;
 };
@@ -60,45 +42,57 @@ const connectDB = () => {
 router.post('/', async (req: Request, res: Response) => {
   const data: Entry = req.body;
 
-  if (data && Object.keys(data).length === 0) {
-    return res.status(404).json({ message: 'Not Found - [404]' });
-  }
+  if (data && Object.keys(data).length === 0) return res.status(404).json({ message: 'Not Found - [404]' });
 
-  // fakeDb.push(data);
-
-  const db = connectDB();
-  const accounts = db.collection('accounts');
-  accounts.insertOne(data);
-  client.close();
-
-  // fs.writeFile(path.join(__dirname, 'data.json'), JSON.stringify(fakeDb), (err) => {
-  //   console.log(err);
-  // });
-  // const client = connectDB();
-  // try {
-  // } finally {
-  //   await client.close();
-  // }
-
-  return res.json(data);
-});
-
-router.get('/', (req, res) => {
-  if (!fakeDb) return res.status(404).json({ message: 'Not Found - [404]' });
-  return res.json({ message: fakeDb });
-});
-
-router.get('/:id', (req, res) => {
-  const id = req.params.id;
-  if (id) {
-    // const account = fakeDb.filter((acc) => acc.id === id);
-    // if (account && Object.keys(account).length > 0) return res.json(account);
-    const db = connectDB();
+  try {
+    const db = await connectDB();
     const accounts = db.collection('accounts');
-    const data = accounts.findOne({ _id: id });
-    client.close();
-    return res.status(200).json(data);
+    await accounts.insertOne(data);
+
+    return res.status(201).json(data);
+  } catch (err) {
+    console.log(err);
+  } finally {
+    await client.close();
+    console.log('Connection close.');
   }
+  return res.status(500).json({ message: 'Internal Server Error - [500]' });
+});
+
+router.get('/', async (req, res) => {
+  try {
+    const db = await connectDB();
+    const accounts = await db.collection('accounts').find({}).toArray();
+
+    return res.status(200).json(accounts);
+  } catch (err) {
+    console.log(err);
+  } finally {
+    await client.close();
+    console.log('Connection close.');
+  }
+
+  return res.status(404).json({ message: 'Not Found - [404]' });
+});
+
+router.get('/:id', async (req, res) => {
+  const id = req.params.id;
+
+  if (id) {
+    try {
+      const db = await connectDB();
+      const accounts = db.collection('accounts');
+      const data = accounts.findOne({ _id: id });
+
+      return res.status(200).json(data);
+    } catch (err) {
+      console.log(err);
+    } finally {
+      await client.close();
+      console.log('Connection close.');
+    }
+  }
+
   return res.status(404).json({ message: 'Not Found - [404]' });
 });
 
@@ -121,7 +115,6 @@ router.put('/:id', (req, res) => {
 router.delete('/:id', (req, res) => {
   const id = req.params.id;
   if (id) {
-    // refazer com pop
     const newDb = fakeDb.filter((db) => db.id !== id);
     fs.writeFile(path.join(__dirname, 'data.json'), JSON.stringify(newDb), (err) => {
       console.log(err);
