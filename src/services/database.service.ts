b12925db807f605641c431b5e75997f3f87d330e
gcamposl <gcamposlopes@gmail.com>
2022-08-29 11:21:28 -0300

    config database

--- src/services/database.service.ts
+++ src/services/database.service.ts
@@ -8,3 +8,16 @@ import * as dotenv from 'dotenv';
 // Initialize Connection
 
 export const collections: { accounts?: mongoDB.Collection } = {};
+
+export async function connectToDatabase() {
+  dotenv.config();
+  const client: mongoDB.MongoClient = new mongoDB.MongoClient(process.env.DB_CONN_STRING);
+  await client.connect();
+  const db: mongoDB.Db = client.db(process.env.DB_NAME);
+  const accountsCollection: mongoDB.Collection = db.collection(process.env.ACCOUNTS_COLLECTION_NAME);
+  collections.accounts = accountsCollection;
+
+  console.log(
+    `Successfully connected to database: ${db.databaseName} and collection: ${accountsCollection.collectionName}`
+  );
+}
